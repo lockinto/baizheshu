fe8e6cec7dc62e7d17085010f49f017b933fb2c3
lockinto <924354349@qq.com>
2015-11-04 18:46:39 +0800

    新建了java web项目

---
+++ baizheshu/src/main/java/STUCO/baizheshu/App.java
@@ -0,0 +1,13 @@
+package STUCO.baizheshu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
